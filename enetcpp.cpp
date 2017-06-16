a305bd2ec763501b075d4fae4eca401b663713e5
Ian Diaz <shadowndacorner@gmail.com>
2017-06-16 14:21:50 -0500

    Added cpp file for msvc compilation

---
+++ enetcpp.cpp
@@ -0,0 +1,12 @@
+extern "C"
+{
+	#include "callbacks.c"
+	#include "compress.c"
+	#include "host.c"
+	#include "list.c"
+	#include "packet.c"
+	#include "peer.c"
+	#include "protocol.c"
+	#include "unix.c"
+	#include "win32.c"
+}
